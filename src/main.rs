7c8e296c1773008770754d1418b1e84dd932fa60
Serpent03 <dumbpilot03@gmail.com>
2024-04-26 11:42:17 +0530

    feat: init vid download setup

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,27 @@
 // TODO enable multi-threading(low priority)
 
 use std::io::{ self, BufRead, BufWriter };
+use rustube::Video;
+
+// FIX returning promise/future for the video download
+fn get_audio(url: &str) -> _ {
+    return Video::from_id(url); 
+}
 
 fn main() {
-    // get the MVP up and running. should be able to deal with I/O and make web requests.
-    
-    println!("Reading input five times.");
+    println!("Starting download!");
     let stdin = io::stdin();
     let mut line:String = String::new();
 
-    for _i in 0..5 {
-        /* _i is named as such because even though we need it, we don't end up using it. */
-        stdin.lock().read_line(&mut line).unwrap();
-        println!("The string is: {}", line);
-        line.clear();
-    }
+    // one thread handles music and another thread handles play/pause
+    // for _i in 0..5 {
+    //     stdin.lock().read_line(&mut line).unwrap();
+    //     println!("The string is: {}", line);
+    //     line.clear();
+    // }
+    
+    let url: &str = "https://youtu.be/nv2wQvn6Wxc";
+    let path_to_video = rustube::blocking::download_best_quality(url);
+
+    println!("Video Downloaded!");
 }
